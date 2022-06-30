6d5090e4227a5ebfe1bbd660d8596f6a1b2c5eb8
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-06-30 15:12:49 +0600

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import bg from './Assets/bg.jpg';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <section className='overflow-hidden bg-no-repeat bg-cover bg-fixed h-screen' style={{ backgroundImage: `url(${bg})` }}>
+      <Navbar />
+    </section>
   );
 }
 
