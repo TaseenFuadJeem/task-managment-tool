6de3fadd880f353040f900e6af65c277c1284d66
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-06-30 16:05:03 +0600

    Added button to to-fo list

--- src/Components/To-do/Todo.js
+++ src/Components/To-do/Todo.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { MdPostAdd } from 'react-icons/md';
 
 const Todo = () => {
     return (
-        <div>
-            This is Todo
-        </div>
+        <section className='mt-20 lg:mt-28 lg:px-32 px-3'>
+            <div className='lg:flex lg:justify-between lg:items-center'>
+                <div>
+                    <h1 className='lg:text-5xl text-4xl ' style={{ fontFamily: 'Russo One' }}>Your To-Do List</h1>
+                    <span className='flex'>
+                        <div className='w-14 border-t-8 border-gray-700'></div>
+                        <div className='lg:w-96 w-64 mt-1 border-t-2 border-gray-700'></div>
+                    </span>
+                </div>
+                <div className='flex justify-center mt-7 lg:mt-0'>
+                    <button className='btn btn-primary'>Add a task <MdPostAdd className='ml-2 text-2xl' /></button>
+                </div>
+            </div>
+        </section>
     );
 };
 
