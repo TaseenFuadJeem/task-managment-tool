db94aef07389782f8da7d57ac315a82a2f2b949a
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-07-01 06:31:24 +0600

    Added completed tasks

--- src/Components/CompletedTask/CompletedTasks.js
+++ src/Components/CompletedTask/CompletedTasks.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Loading/Loading';
 
 const CompletedTasks = () => {
+
+    const { data: tasks, isLoading } = useQuery("CompletedTasks", () => fetch("http://localhost:5000/completed-tasks?task=completed").then(res => res.json()));
+
+    if (isLoading) {
+        return <Loading />
+    }
+
     return (
-        <div>
-            This is Completed task
+        <div className='mt-20 lg:mt-28 lg:px-32 px-3'>
+            <div>
+                <h1 className='lg:text-5xl text-4xl ' style={{ fontFamily: 'Russo One' }}>Completed Tasks</h1>
+                <span className='flex'>
+                    <div className='w-14 border-t-8 border-gray-700'></div>
+                    <div className='lg:w-96 w-64 mt-1 border-t-2 border-gray-700'></div>
+                </span>
+            </div>
+
+            <div class="grid lg:grid-cols-3 gap-9 mt-10">
+                {
+                    tasks?.map(task =>
+                        <div className='min-h-[400px] p-4 rounded-lg shadow-2xl bg-white' key={task._id}>
+                            <h1>Title: <span className='text-2xl font-bold'>{task.title}</span></h1>
+                            <h2 className='my-2'>Date: {task.date}</h2>
+                            <p>{task.details}</p>
+                        </div>
+                    )
+                }
+            </div>
         </div>
     );
 };
--- src/Components/Home/EachTask.js
+++ src/Components/Home/EachTask.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const EachTask = ({ task, index }) => {
+
+    const completeTask = () => {
+
+        const url = `http://localhost:5000/task-complete/${task._id}`
+
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ task: "completed" })
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+                toast.success("Task updated successfully 😄");
+            });
+
+    }
+
     return (
         <div className='min-h-[400px] p-4 rounded-lg shadow-2xl bg-white'>
             <div className='flex justify-between mb-4'>
-                <input type="checkbox" class="checkbox checkbox-lg" />
+                <input onClick={() => completeTask()} type="checkbox" class="checkbox checkbox-lg" />
                 <p className='text-lg font-semibold'>Note: {index + 1}</p>
             </div>
             <h1>Title: <span className='text-2xl font-bold'>{task.title}</span></h1>
